71fe2f7fbbaffd6a1b7cb502683e47e6f397d2e3
hasib <hasibulhossain52@gmail.com>
2021-10-31 14:36:55 +0600

    update

--- app.js
+++ app.js
@@ -2,61 +2,53 @@ const CSVtoJSON = require('csvtojson');
 const JSONtoCSV = require('json2csv').parse;
 const fs = require('fs-extra');
 
-const products = CSVtoJSON().fromFile("./products.csv");
-const imageUrl = CSVtoJSON().fromFile("./image_url.csv"); 
+const productsCSV = CSVtoJSON().fromFile("./products.csv");
+const imageUrlCSV = CSVtoJSON().fromFile("./image_url.csv"); 
 
+// Recommended way to merge multiple product img url with product;
 
-// Recommended way to merge product img url with product;
-
-Promise.all([products, imageUrl]).then(values => {
+Promise.all([productsCSV, imageUrlCSV]).then(values => {
     const matched = [];
     const unmatched = [];
 
     const products = values[0];
     const imagesUrl = values[1];
 
-    products.reverse().forEach(product => {
-        const index = imagesUrl.findIndex(imgUrl => product.product_name === imgUrl.img_url.split('--')[0]);
+    products.forEach(product => {
+        const index = imagesUrl.findIndex(imgUrl => product.product_name === imgUrl.img_url.split('---')[0]);
+        
+        const matchedPhotos = imagesUrl.filter(imgUrl => {
+            
+            const imageUrl = imgUrl.img_url.split('---')[0];
+            const pn = product.product_name;
+            
+            if(pn === imageUrl || pn + '-1' === imageUrl || pn + '-2' === imageUrl || pn + '-3' === imageUrl || pn + '-4' === imageUrl || pn + '-5' === imageUrl) {
+                return true
+            } else {
+                return false
+            }
+        }).map(item => {
+            return item.img_url
+        })
 
+        const featured_img = matchedPhotos.filter(img_url => product.product_name.includes(img_url.split('---')[0]));
+        const regular_img = matchedPhotos.filter(img_url => !product.product_name.includes(img_url.split('---')[0]));
+        
+        
         if(index !== -1) {
-            matched.push({...product, img_url: imagesUrl[index].img_url})
+            matched.push({...product, featured_img: featured_img.join(''), product_img: regular_img.join(',')})
         } else {
             unmatched.push({product_name: product.product_name})
         }
-    })
 
-    const matchedCsv = JSONtoCSV(matched, { fields: ["product_name", "price", "img_url"] });
-    fs.writeFile("./final-product.csv", matchedCsv).then(_ => console.log('matched successful')).catch(_ => console.log('something went wrong!'))
+        console.log(`Product name: ${product.product_name}, Image found: ${matchedPhotos.length}`);
+    })
+    console.log('-------------------------\n')
+
+    const matchedCsv = JSONtoCSV(matched, { fields: ["product_name", "price", "featured_img", "product_img"] });
+    fs.writeFile("./final-product.csv", matchedCsv).then(_ => console.log(`complete successfully: ${matched.length} products`)).catch(_ => console.log('something went wrong!'))
     
     const unmatchedCsv = JSONtoCSV(unmatched, { fields: ["product_name"] });
-    fs.writeFile("./final-product-unmatched.csv", unmatchedCsv).then(_ => console.log('unmatched successful')).catch(_ => console.log('something went wrong!'))
+    fs.writeFile("./final-product-unmatched.csv", unmatchedCsv).then(_ => console.log(`Have problem with ${unmatched.length} products`)).catch(_ => console.log('something went wrong!'))
     
 })
-
-
-
-// Another way to merge product url with product. But there is no support for unmatched img url handling
-
-// Promise.all([products, imageUrl]).then(values => {
-//     let finalizeArr;
-//     const products = values[0];
-//     const imagesUrl = values[1];
-
-//     finalizeArr = imagesUrl.reverse().map(imageUrl => {
-
-//         const imgName = imageUrl.img_url.split('--')[0]
-
-//         for (let index = 0; index < products.length; index++) {
-//             const product = products[index];
-
-//             if(product.product_name === imgName) {
-//                 product.img_url = imageUrl.img_url
-//                 return product;
-//             }
-//         }
-
-
-//     })
-
-//     console.log(finalizeArr.filter(item => item !== undefined))
-// })
